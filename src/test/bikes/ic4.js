ae514a26d6241416838312aff47c798af62a7d2e
4o4R <120294988+4o4R@users.noreply.github.com>
2025-10-28 22:15:04 -0500

    git

--- src/test/bikes/ic4.js
+++ src/test/bikes/ic4.js
@@ -2,9 +2,9 @@ import test from 'tape';
 import {parse} from '../../bikes/ic4.js';
 
 test('parse() parses Schwinn IC4 indoor bike data values', t => {
+  t.plan(2);
   const buf = Buffer.from('4402da020201220100', 'hex');
   const {power, cadence} = parse(buf);
   t.equal(power, 290, 'power (watts)');
   t.equal(cadence, 129, 'cadence (rpm)');
-  t.end();
 });
