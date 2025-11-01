89f35902c2be0eb0bfe2a5c7d193fb8665ef84c0
4o4R <120294988+4o4R@users.noreply.github.com>
2025-10-31 22:03:45 -0500

    fix sinon

--- src/test/support/sinon.js
+++ src/test/support/sinon.js
@@ -5,6 +5,9 @@ const require = createRequire(import.meta.url);
 let sinon;
 try {
   sinon = require('sinon');
+  if (sinon && typeof sinon === 'object' && 'default' in sinon) {
+    sinon = sinon.default;
+  }
 } catch (error) {
   throw new Error(
     "Cannot load the 'sinon' package. Install development dependencies first: npm install --include=dev"
